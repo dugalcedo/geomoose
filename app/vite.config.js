c5f3c87a5ae4a9504a324900e9c292301f055ace
dugalcedo <107317025+dugalcedo@users.noreply.github.com>
2023-11-10 11:19:04 +0100

    add geomoose to publicdir

--- app/vite.config.js
+++ app/vite.config.js
@@ -2,9 +2,19 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  build: {
-    outDir: '../'
+export default defineConfig(({mode}) => {
+  if (mode === 'development'){
+    return {
+      plugins: [vue()],
+      build: {
+        outDir: '../'
+      }
+    }
+  } else return {
+    plugins: [vue()],
+    build: {
+      outDir: '../'
+    },
+    publicDir: './geomoose'
   }
 })
