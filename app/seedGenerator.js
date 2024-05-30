8c08416182d88d29c9266f1af915a1db1aa0a5a3
dugalcedo <107317025+dugalcedo@users.noreply.github.com>
2024-05-30 11:54:22 +0200

    gen 4k quizzes

--- app/seedGenerator.js
+++ app/seedGenerator.js
@@ -1,7 +1,7 @@
 import { writeFileSync } from 'fs'
 import quizBot from "./src/lib/quiz/quiz.js"
 const seeds = []
-for (let i = 0; i < 366; i++) {
+for (let i = 0; i < 4000; i++) {
     seeds.push(quizBot.generateRandomQuizSeed())
 }
-writeFileSync('./src/lib/quizzes.js', 'export default ' + JSON.stringify(seeds))
+writeFileSync('./src/lib/quizzes.js', 'export default ' + JSON.stringify(seeds, null, 2))
